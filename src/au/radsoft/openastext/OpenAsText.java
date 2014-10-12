16df355532a4c14145998e6f86b0c30d2b06997e
RadAd <adamgates84+github@gmail.com>
2014-10-12 16:53:50 +1100

    Initial version

---
+++ src/au/radsoft/openastext/OpenAsText.java
@@ -0,0 +1,30 @@
+package au.radsoft.openastext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class OpenAsText extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        Intent origIntent = getIntent();
+		
+        Intent newIntent = new Intent(origIntent.getAction());
+        if (origIntent.getCategories() != null)
+        {
+            //for (String category : origIntent.getCategories())
+                //newIntent.addCategory(category);
+        }
+		newIntent.setDataAndType(origIntent.getData(), "text/plain");
+        newIntent.setFlags(origIntent.getFlags());
+        newIntent.putExtras(origIntent.getExtras());
+        
+        startActivity(newIntent);
+		finish();
+    }
+}
+
